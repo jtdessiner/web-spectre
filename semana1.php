60d69e5ede3a924d5da1de214f31482830dd19e9
jtdessiner <joseto.valverde@gmail.com>
2022-12-22 21:04:50 +0100

    se fue la pagina por el cambio index.php, ahora está index.html

--- semana1.php
+++
@@ -1,3 +0,0 @@
-<?php get_header(); ?>
-<h1>ESTA ES LA PRIMERA SEMANA</h1>
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem eligendi illo non dolorum, sequi quis consequatur architecto saepe iste reprehenderit, voluptatum reiciendis, unde provident ullam expedita nesciunt quibusdam doloremque facere!</p>
