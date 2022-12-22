5603ac37c12550d10205381cd7ad4a9c32d2bf0b
jtdessiner <joseto.valverde@gmail.com>
2022-12-22 20:48:27 +0100

    añado pagina

---
+++ semana1.php
@@ -0,0 +1,3 @@
+<?php get_header(); ?>
+<h1>ESTA ES LA PRIMERA SEMANA</h1>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem eligendi illo non dolorum, sequi quis consequatur architecto saepe iste reprehenderit, voluptatum reiciendis, unde provident ullam expedita nesciunt quibusdam doloremque facere!</p>
